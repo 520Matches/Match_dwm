6630195d56aea92a85538e463f64eee9f360332a
520Matches <HengDi_Sun@163.com>
2020-09-30 15:34:08 +0800

    add rofi use mode+r

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -78,7 +78,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *cmd_feh[] = {"/bin/sh", "~/.config/feh.sh", NULL};
+// static const char *cmd_feh[] = {"/bin/sh", "~/.config/feh.sh", NULL};
+static const char *roficmd[] = {"rofi", "-show", "run"};
 
 
 static Key keys[] = {
@@ -86,6 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,						XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_v,      spawn,          SHCMD("~/.config/feh.sh") },
+	{ MODKEY,                       XK_r,      spawn,          {.v = roficmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
